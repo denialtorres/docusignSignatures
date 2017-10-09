b81974fed18c31f15b04b88def2831236d68a355
denialtorres <denial.torres@gmail.com>
2017-10-09 21:46:53 +0000

    configure with postqress

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171009174248) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "envelope_users", force: :cascade do |t|
     t.integer "envelope_id"
     t.integer "user_id"
